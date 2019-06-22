6ebfb1ea027c43fc7db37686e83707a129b34823
itay livni <livni.itay@gmail.com>
2019-06-22 19:58:59 +0000

    txt_to_corpus added

--- prepare_meta_data/write.py
+++ prepare_meta_data/write.py
@@ -3,19 +3,24 @@ Loads datasets to train algorithms and generate images for supported text to ima
 
 
 """
-from collections import namedtuple
 import pandas as pd
+import sys
 import os
 import shutil
 import requests
 from io import BytesIO
 from zipfile import ZipFile
-import textacy
-
 import glob
+
+import textacy
 import en_core_web_sm
 
-import prepare_meta_data as pmd
+from prepare_meta_data import read
+
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 
 def zip_to_metadata_dir(zip_url, data_dir_path):
@@ -24,7 +29,7 @@ def zip_to_metadata_dir(zip_url, data_dir_path):
     Returns a zipfile object
     """
     # Returns a zipped directory
-    zipfile = pmd.read.zip_from_url(zip_url)
+    zipfile = read.zip_from_url(zip_url)
 
     if not os.path.exists(data_dir_path):
         os.mkdir(data_dir_path)
@@ -35,16 +40,28 @@ def zip_to_metadata_dir(zip_url, data_dir_path):
     return zipfile
 
 
-def txt_to_corpus(txt_dir):
+def txt_to_corpus(txt_dir, txt_extention=".txt"):
     """
     Reads a text directory and puts in a textacy corpus with the filename as metadata
     """
+    # Get the name of the function - should be decorator for every function
+    functionNameAsString = sys._getframe().f_code.co_name
+
+
+    # Load english language model for sents parsing and caption relabeling
     en = en_core_web_sm.load()
+
+    # Initalize spacy corpus using textacy
     imageLabels_corpus = textacy.corpus.Corpus(en)
 
-    for f in glob.glob(txt_dir + "*.txt"):
+    # Loop throuh the text directory (input), for all the files ending with .txt
+    search_dir = f"{txt_dir}/*{txt_extention}"
+    logging.debug(f"Function: {functionNameAsString} -- Loading Text from: {search_dir}")
+    for f in glob.glob(search_dir):
         txt_str = open(f).read()
         imageLabels_corpus.add_record((txt_str, {"file_name": f})  )
+
+    logging.debug(f"Function: {functionNameAsString} -- Loaded {imageLabels_corpus.n_docs}")
 
     return imageLabels_corpus
 
