74b97e02443a70622fa61a39f0b541dd1c8a7473
itay livni <livni.itay@gmail.com>
2019-07-02 06:22:52 +0000

    finished writing prepare basenames

--- pytig/filenames.py
+++ pytig/filenames.py
@@ -18,14 +18,18 @@ class PrepareFilenames():
     - renames filenams
     - writes filename.txt to metadata folder
     """
-    def __init__(self, image_training_data_flpth, text_training_data_flpth, **kwargs):
+    def __init__(self, metadata_flpth, image_training_data_flpth, text_training_data_flpth, **kwargs):
 
         # File paths to data directories
+        self.metadata_flpth = metadata_flpth
+
         self.image_training_data_flpth = image_training_data_flpth
         self.text_training_data_flpth = text_training_data_flpth
 
         self.txt_dir = os.path.basename(self.text_training_data_flpth)
         self.img_dir = os.path.basename(self.image_training_data_flpth)
+
+        self.basenameCol = kwargs.setdefault('filename_clmn','filename')
 
         # Load file names to df from input directories (with explit extentions)
         txt_ext = kwargs.setdefault('txt_ext', ".txt")
@@ -33,11 +37,11 @@ class PrepareFilenames():
         self.fileNames_df = write.filenames_to_df(self.image_training_data_flpth, self.text_training_data_flpth, txt_ext=txt_ext, img_ext=img_ext)
 
         # Extract filenames for manipulation and to write the pickle filenames
-        self.basenameCol = 'filename'
+
         self.fileNames_df[self.basenameCol] = self.extract_filenames(self.fileNames_df)
 
     def extract_filenames(self, df, flpthColNms_lst=['images','text']):
-        """Extracts the base file names from the txt and image filepaths to a list"""
+        """Extracts the base filenames from the txt and image filepaths to a list"""
 
         # Stack the text and column images flpth to one column
         flpths_df = df[flpthColNms_lst].stack()
@@ -53,7 +57,7 @@ class PrepareFilenames():
         return baseNm_series
 
 
-    def rename_filenames(self, preprocess_text=True, _enumerate=False, **kwargs):
+    def rename_basename(self, preprocess_text=True, _enumerate=False, **kwargs):
         """
         Applies textacy text preprocess to each filename with various options.  Enumerates filenames if enumerate_ is true
 
@@ -81,14 +85,15 @@ class PrepareFilenames():
         if _enumerate:
             self.fileNames_df[self.basenameCol] = self.fileNames_df[self.basenameCol].str.cat(self.fileNames_df.index.values.astype(str), sep="_")
 
+        logging.info(f"Finished renaming basenames")
 
         return self.fileNames_df
 
-    def rename_source_filenames(self):
+    def rename_filenames(self):
         """
         Write filenames back to disk with new filenames taken from the filename_df basenameCol
         """
-        # Reshape to filenames to long form so all the original filepaths are in a list as opposed to tw columns
+        # 1. Reshape to filenames to long form so all the original filepaths are in a list as opposed to tw columns
         filename_df  = pd.melt(self.fileNames_df, id_vars=[self.basenameCol], var_name='filetype', value_name='orig_filepath')
 
         def rename_file(row):
@@ -101,20 +106,26 @@ class PrepareFilenames():
 
             os.rename(row['orig_filepath'], new_filepath)
 
-        # Write the fikenames back to disk with stacked df
+        # 2. Write the filenames back to disk with stacked df
         filename_df.apply(lambda row: rename_file(row), axis=1)
+        logging.info(f"Finished writing new image and text filenames to disk")
 
-        # reset all the columns to match the filenames on disk
+        # 3. reset all the columns text and image source filenames to match the filenames on disk
         self.fileNames_df[self.basenameCol] = self.extract_filenames(self.fileNames_df)
         return
 
-    def make_filenames_txtfile(self):
+    def basenames_to_txtfile(self, basename_flname='filenames.txt'):
+        """
+        Write filenamee basenames to a txt file
+        """
+        write_filename_path = os.path.join(self.metadata_flpth, basename_flname)
+
+        self.fileNames_df.to_csv(write_filename_path,
+                                  columns=[self.basenameCol],
+                                  index=False,
+                                  header=False
+                                  )
+
+        logging.debug(f"Finished writing filenames to: {write_filename_path} Number of Basenames: {self.fileNames_df[self.basenameCol].shape}")
 
         return
-
-
-
-
-
-
-
