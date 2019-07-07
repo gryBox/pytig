1e3d7d3bf06f39402eb187cba311eb4ba790d1bb
itay livni <livni.itay@gmail.com>
2019-07-07 03:43:13 +0000

    replaced calcStas to calcStats returns df

--- pytig/captions/corpus_stats.py
+++ pytig/captions/corpus_stats.py
@@ -4,6 +4,7 @@ TODO: Add max and other stats.  Should this be extended to include those docs?
 """
 from pprint import pformat
 import textacy
+import pandas as pd
 # doc extentions are a concept introduced in textacy 0.7.0
 textacy.spacier.doc_extensions.set_doc_extensions()
 
@@ -20,12 +21,13 @@ class CorpusStats:
         TODO:  Add other stats e.g. mean, std
         """
 
-        self.min_sents = self.minSents(corpus)
-        self.max_sents = self.maxSents(corpus)
+        # self.min_sents = self.minSents(corpus)
+        # self.max_sents = self.maxSents(corpus)
 
-        self.min_tokens = self.minTokens(corpus)
-        self.max_tokens = self.maxTokens(corpus)
+        # self.min_tokens = self.minTokens(corpus)
+        # self.max_tokens = self.maxTokens(corpus)
 
+        self.docstats_df = self.calc_docstats_df(corpus)
         logger.debug(f"Finished calculating CorpusStats")
 
 
@@ -47,7 +49,30 @@ class CorpusStats:
         min_tokens = max([doc._.n_tokens for doc in corpus])
         return min_tokens
 
+    def calc_doc_stats(self, doc):
 
+        doc_df = pd.DataFrame()
+
+        # Initializes dictionary with doc metadata
+        doc_record = doc._.meta
+
+        # Add basic counts i.e m_words
+        ts =  textacy.text_stats.TextStats(doc)
+        doc_record.update(ts.basic_counts)
+
+        doc_df = doc_df.append(doc_record, ignore_index=True)
+        return doc_df
+
+    def calc_docstats_df(self, crps):
+
+        # Get a list of records with doc stats for df
+        docstat_lst = [self.calc_doc_stats(doc) for doc in crps]
+
+        # Load into df
+        docstats_df = pd.concat(docstat_lst, axis=0, ignore_index=True)
+
+
+        return docstats_df
 
 
     def __repr__(self):
